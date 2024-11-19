fccdcdf132e122ae432616d60439403edff274e6
Janarthanan <janajana9047@gmail.com>
2024-11-19 10:55:49 +0530

    commit

--- RealEstateAPI/merge.js
+++ RealEstateAPI/merge.js
@@ -2,13 +2,17 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path=require('path')
 
 const app = express();
 const port = 9091; // Single port for the merged application
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname, '../RealEstate')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../RealEstate', 'RealEstate.html'));
+});
 // MySQL database connection
 const db = mysql.createConnection({
     host: 'localhost',
